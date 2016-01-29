19e6005c5d38952d713226c9ccafdb798e5268fc
richard <lichard49@gmail.com>
2016-01-29 11:52:32 -0500

    Implemented dynamic date retrieval

--- utility.js
+++ utility.js
@@ -1,8 +1,4 @@
-function buildCalendar() {
-  var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-  var dates = ['1/18', '1/19', '1/20', '1/21', '1/22'];
-  var hours = ['9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM'];
-  
+function buildCalendar(days, dates, hours) {
   var calendarContents = '<tr><th></th>';
   var day;
   for(day in days) {
